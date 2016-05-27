d967ecd5775f1500a97f90151120fd69c85d3428
Daniel Martín-Albo <damarsi1@upv.es>
2016-05-27 16:25:17 +0200

    Fixed Model definition

--- src/Model.lua
+++ src/Model.lua
@@ -6,7 +6,7 @@ function createModel(sample_height, num_labels)
   local ks = {3, 3, 3, 3, 3, 3, 2}
   local ss = {1, 1, 1, 1, 1, 1, 1}
   local nm = {16, 16, 32, 32, 64, 128, 128}
-  local nh = {256, 256, 256}
+  local nh = {256}
 
   function convBlock(depth_in, depth_out, size, stride, batch_norm)
     batch_norm = batch_norm or false
@@ -37,18 +37,19 @@ function createModel(sample_height, num_labels)
   model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
   model:add(convBlock(nm[2], nm[3], ks[3], ss[3], true))
   model:add(convBlock(nm[3], nm[4], ks[4], ss[4]))
-  model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+  model:add(nn.SpatialMaxPooling(2, 2, 2, 2))             -- 32 x 8
   
   -- RNN part
   model:add(nn.Transpose({2,4},{1,2}))
   model:add(nn.Contiguous())
   model:add(nn.Reshape(-1,256,true))
   model:add(nn.Dropout(0.5))
-  model:add(cudnn.BiLSTM(256, 256, 3, nil, 0.5))
+  --model:add(cudnn.BiLSTM(nh[1], nh[1], 3, nil, 0.5))
+  model:add(cudnn.BGRU(256, nh[1], 3, nil, 0.5))
   model:add(nn.Dropout(0.5))
   model:add(nn.Contiguous())  
-  model:add(nn.Reshape(-1, 512, false))
-  model:add(nn.Linear(512, num_labels + 1))
+  model:add(nn.Reshape(-1, nh[1]*2, false))
+  model:add(nn.Linear(nh[1]*2, num_labels + 1))
 
   return model
 end
