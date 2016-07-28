828e6ee82af77ddbe8f956f8858b76c686e624c7
Mauricio Villegas <mauvilsa@upv.es>
2016-07-28 14:40:57 +0200

    Modified batchers and tools so that the data is provided as a list of images

--- train.lua
+++ train.lua
@@ -12,8 +12,9 @@ require 'optim'
 require 'xlua'
 
 -- TODO: Curriculum batcher does not work properly
--- require 'src.CurriculumBatcher'
-require 'src.RandomBatcher'
+-- require 'src.CurriculumBatcher'; local Batcher = CurriculumBatcher
+-- TODO: The type of batcher could be selected as an arg when calling train.lua
+require 'src.RandomBatcher'; local Batcher = RandomBatcher
 
 require 'src.utilities'
 local opts = require 'src.TrainOptions'
@@ -73,74 +74,80 @@ print('Number of parameters: ', gradParameters:nElement())
 -- Perform forward (and optionally, backprop) pass. This is common
 -- code used for both training and evaluation of the model.
 local fb_pass = function(batch_img, batch_gt, do_backprop)
-   do_backprop = do_backprop or false
-   if do_backprop then
-      model:training()
-   else
-      model:evaluate()
-   end
+  do_backprop = do_backprop or false
+  if do_backprop then
+    model:training()
+  else
+    model:evaluate()
+  end
 
-   gradParameters:zero()      -- Reset parameter gradients to 0
-   model:forward(batch_img)   -- Forward pass
+  gradParameters:zero()      -- Reset parameter gradients to 0
+  model:forward(batch_img)   -- Forward pass
 
-   local output = model.output
-   local sizes = {}
-   local seq_len = output:size()[1] / opt.batch_size
-   for i=1,opt.batch_size do table.insert(sizes, seq_len) end
+  local output = model.output
+  local sizes = {}
+  local seq_len = output:size()[1] / opt.batch_size
+  for i=1,opt.batch_size do table.insert(sizes, seq_len) end
 
-   local grad_output = output:clone():zero()
-   local loss = 0
+  local grad_output = output:clone():zero()
+  local loss = 0
 
-   -- Compute loss function and gradients respect the output
-   if opt.gpu >= 0 then
-      loss = table.reduce(gpu_ctc(output, grad_output, batch_gt, sizes),
-			  operator.add, 0)
-   else
-      output:float()
-      grad_output:float()
-      loss = table.reduce(cpu_ctc(output, grad_output, batch_gt, sizes),
-			  operator.add, 0)
-   end
+  -- Compute loss function and gradients respect the output
+  if opt.gpu >= 0 then
+    loss = table.reduce(gpu_ctc(output, grad_output, batch_gt, sizes),
+                        operator.add, 0)
+  else
+    output:float()
+    grad_output:float()
+    loss = table.reduce(cpu_ctc(output, grad_output, batch_gt, sizes),
+                        operator.add, 0)
+  end
 
-   -- Perform framewise decoding to estimate CER
-   local batch_decode = framewise_decode(opt.batch_size, output)
-   local batch_num_edit_ops = 0
-   local batch_ref_length = 0
-   for i=1,opt.batch_size do
-      local num_edit_ops, _ = levenshtein(batch_decode[i], batch_gt[i])
-      batch_num_edit_ops = batch_num_edit_ops + num_edit_ops
-      batch_ref_length = batch_ref_length + #batch_gt[i]
-   end
+  -- Perform framewise decoding to estimate CER
+  local batch_decode = framewise_decode(opt.batch_size, output)
+  local batch_num_edit_ops = 0
+  local batch_ref_length = 0
+  for i=1,opt.batch_size do
+    local num_edit_ops, _ = levenshtein(batch_decode[i], batch_gt[i])
+    batch_num_edit_ops = batch_num_edit_ops + num_edit_ops
+    batch_ref_length = batch_ref_length + #batch_gt[i]
+  end
 
-   -- Make loss function (and output gradients) independent of batch size
-   -- and sequence length.
-   loss = loss / (opt.batch_size * seq_len)
-   grad_output:div(opt.batch_size * seq_len)
+  -- Make loss function (and output gradients) independent of batch size
+  -- and sequence length.
+  loss = loss / (opt.batch_size * seq_len)
+  grad_output:div(opt.batch_size * seq_len)
 
-   -- Compute gradients of the loss function w.r.t parameters
-   if do_backprop then
-      model:backward(batch_img, grad_output)
-   end
+  -- Compute gradients of the loss function w.r.t parameters
+  if do_backprop then
+    model:backward(batch_img, grad_output)
+  end
 
-   return loss, batch_num_edit_ops, batch_ref_length
+  return loss, batch_num_edit_ops, batch_ref_length
 end
 -- END fb_pass
 
 -- Determine the filename of the output model
 local output_model_filename = opt.model
 if opt.output_model ~= '' then
-   output_model_filename = opt.output_model
+  output_model_filename = opt.output_model
 end
 
 local output_progress_file = nil
 local progress_header = '# EPOCH   BEST?   TRAIN_LOSS   VALID_LOSS   TRAIN_CER   VALID_CER   TRAIN_TIME   VALID_TIME\n'
 if opt.output_progress ~= '' then
-   output_progress_file = io.open(opt.output_progress, 'w')
-   output_progress_file:write(progress_header)
+  output_progress_file = io.open(opt.output_progress, 'w')
+  output_progress_file:write(progress_header)
+  output_progress_file:flush()
+else
+  io.stdout:write(progress_header)
+  io.stdout:flush()
 end
 
-local dt = RandomBatcher(opt.training, true)
-local dv = RandomBatcher(opt.validation, true)
+opt.gt_file = opt.training_gt
+local dt = Batcher(opt.training, opt)
+opt.gt_file = opt.validation_gt
+local dv = Batcher(opt.validation, opt)
 
 -- Keep track of the performance on the training data
 local train_loss_epoch = 0.0
@@ -174,7 +181,7 @@ local current_criterion_value = {
 }
 assert(current_criterion_value[opt.early_stop_criterion] ~= nil,
        string.format('Early stop criterion %q is not supported!',
-		     opt.early_stop_criterion))
+                     opt.early_stop_criterion))
 
 ------------------------------------
 -- Main loop
@@ -194,7 +201,7 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
   --    TRAINING EPOCH START    --
   --------------------------------
   local train_time_start = os.time()
-  dt:shuffle()
+  dt:epochReset()
   train_loss_epoch = 0
   train_num_edit_ops = 0
   train_ref_length = 0
@@ -215,19 +222,19 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
       -- Adversarial training
       if opt.adversarial_weight > 0.0 then
         local dJx = model:get(1).gradInput  -- Gradient w.r.t. the inputs
-	-- Distort images so that the loss increases the maximum but the
-	-- pixel-wise differences do not exceed opt.adversarial_epsilon
-	local adv_img = torch.add(batch_img, opt.adversarial_epsilon,
-				  torch.sign(dJx))
+        -- Distort images so that the loss increases the maximum but the
+        -- pixel-wise differences do not exceed opt.adversarial_epsilon
+        local adv_img = torch.add(batch_img, opt.adversarial_epsilon,
+                                  torch.sign(dJx))
 
-	local orig_gradParameters = torch.mul(gradParameters,
-					      1.0 - opt.adversarial_weight)
-	-- Backprop pass to get the gradients respect the adv images
-	local adv_loss, adv_edit_ops, adv_ref_len = fb_pass(adv_img, batch_gt,
-							    true)
-	-- dJ_final = (1 - w) * dJ_orig + w * dJ_adv
-	gradParameters:add(orig_gradParameters,
-			   opt.adversarial_weight, gradParameters)
+        local orig_gradParameters = torch.mul(gradParameters,
+                                              1.0 - opt.adversarial_weight)
+        -- Backprop pass to get the gradients respect the adv images
+        local adv_loss, adv_edit_ops, adv_ref_len = fb_pass(adv_img, batch_gt,
+                                                            true)
+        -- dJ_final = (1 - w) * dJ_orig + w * dJ_adv
+        gradParameters:add(orig_gradParameters,
+                           opt.adversarial_weight, gradParameters)
       end
 
       -- L1 Normalization
@@ -252,7 +259,7 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
     optim.rmsprop(train_batch, parameters, rmsprop_opts)
     -- Show progress bar only if running on a tty
     if isatty then
-       xlua.progress(batch + opt.batch_size - 1, train_num_samples)
+      xlua.progress(batch + opt.batch_size - 1, train_num_samples)
     end
   end
   train_loss_epoch = train_loss_epoch / train_num_samples
@@ -267,14 +274,14 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
   --    VALIDATION EPOCH START    --
   ----------------------------------
   local valid_time_start = os.time()
-  dv:shuffle()
+  dt:epochReset()
   valid_loss_epoch = 0
   valid_num_edit_ops = 0
   valid_ref_length = 0
   for batch=1,valid_num_samples,opt.batch_size do
     local batch_img, batch_gt, batch_sizes = dv:next(opt.batch_size)
     if opt.gpu >= 0 then
-       batch_img = batch_img:cuda()
+      batch_img = batch_img:cuda()
     end
     -- Forward pass
     local batch_loss, batch_num_edit_ops, batch_ref_length =
@@ -285,7 +292,7 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
     valid_ref_length = valid_ref_length + batch_ref_length
     -- Show progress bar only if running on a tty
     if isatty then
-       xlua.progress(batch + opt.batch_size - 1, valid_num_samples)
+      xlua.progress(batch + opt.batch_size - 1, valid_num_samples)
     end
   end
   valid_loss_epoch = valid_loss_epoch / valid_num_samples
@@ -298,23 +305,23 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
   local best_model = false
   local curr_crit_value = current_criterion_value[opt.early_stop_criterion]()
   if best_criterion_value == nil or curr_crit_value < best_criterion_value then
-     best_model = true
-     if best_criterion_value == nil or
-        ((best_criterion_value - curr_crit_value) / best_criterion_value) >= opt.min_relative_improv then
-	last_signif_improv_epoch = epoch
-     end
+    best_model = true
+    if best_criterion_value == nil or
+      ((best_criterion_value - curr_crit_value) / best_criterion_value) >= opt.min_relative_improv then
+      last_signif_improv_epoch = epoch
+    end
   end
 
   if best_model then
-     best_criterion_value = curr_crit_value
-     best_criterion_epoch = epoch
-     local checkpoint = {}
-     checkpoint.train_opt = opt       -- Original training options
-     checkpoint.epoch     = epoch
-     checkpoint.model     = model
-     model:clearState()
-     -- Only save t7 checkpoint if there is an improvement in CER
-     torch.save(output_model_filename, checkpoint)
+    best_criterion_value = curr_crit_value
+    best_criterion_epoch = epoch
+    local checkpoint = {}
+    checkpoint.train_opt = opt       -- Original training options
+    checkpoint.epoch     = epoch
+    checkpoint.model     = model
+    model:clearState()
+    -- Only save t7 checkpoint if there is an improvement in CER
+    torch.save(output_model_filename, checkpoint)
   end
 
   -- Print progress of the loss function, CER and running times
@@ -326,11 +333,11 @@ while opt.max_epochs <= 0 or epoch < opt.max_epochs do
      os.difftime(train_time_end, train_time_start) / 60.0,
      os.difftime(valid_time_end, valid_time_start) / 60.0)
   if output_progress_file ~= nil then
-     output_progress_file:write(progress_line)
-     output_progress_file:flush()
+    output_progress_file:write(progress_line)
+    output_progress_file:flush()
   else
-     io.stdout:write(progress_header)
-     io.stdout:write(progress_line)
+    io.stdout:write(progress_line)
+    io.stdout:flush()
   end
 
   -- Collect garbage every so often
