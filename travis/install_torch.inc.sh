ba373fdd18db9691d62985c28d8d7d4ae4ba1b6a
Joan Puigcerver <joapuipe@gmail.com>
2016-10-19 14:55:19 +0200

    Fix in install_torch.inc.sh

--- travis/install_torch.inc.sh
+++ travis/install_torch.inc.sh
@@ -4,12 +4,18 @@
 export ROOT_TRAVIS_DIR="$(pwd)";
 export TORCH_ROOT="$HOME/torch";
 
-if [[ ! "$TORCH_ROOT" ]]; then git clone https://github.com/torch/distro.git "$TORCH_ROOT" --recursive ; fi
-
+# Make sure that the torch directory is a git clone.
+[[ ! -d "$TORCH_ROOT/.git" ]] && rm -rf "$TORCH_ROOT";
+# If the torch directory does not exist, clone a new repo.
+[[ ! -d "$TORCH_ROOT" ]] && git clone https://github.com/torch/distro.git "$TORCH_ROOT" --recursive ;
+# Update torch
 cd "$TORCH_ROOT";
 git pull && git submodule update && git submodule foreach git pull origin master;
+# Copy torch for a clean install with a particular LUA_VERSION
 cp -a "$TORCH_ROOT" "${TORCH_ROOT}_${TORCH_LUA_VERSION}";
 cd "${TORCH_ROOT}_${TORCH_LUA_VERSION}";
 ./install_torch.sh -b -s;
+# Set environment variables.
 source "${TORCH_ROOT}_${TORCH_LUA_VERSION}/install/bin/torch-activate";
+# Back to travis dir.
 cd "$ROOT_TRAVIS_DIR";
