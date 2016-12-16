bfc4abaabcf63e970d54ff54165d0ffe34518456
Mauricio Villegas <mauvilsa@upv.es>
2016-12-16 12:28:23 +0100

    minor fix in pre-commit hook for versioning

--- laia/Version.lua
+++ laia/Version.lua
@@ -2,7 +2,7 @@ require 'laia.ClassWithOptions'
 
 local Version, Parent = torch.class('laia.Version', 'laia.ClassWithOptions')
 
-Version.DATE = '$Date: 2016-12-16 10:42:30 $'
+Version.DATE = '$Date: 2016-12-16 11:28:23 $'
 
 function Version:registerOptions(parser)
   parser:flag(
