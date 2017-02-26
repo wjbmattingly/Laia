81ad8646fb035f9a82ae65ddc5d55e8ca3a18030
jpuigcerver <joapuipe@gmail.com>
2017-02-26 19:31:53 +0100

    working on IAM scripts

--- egs/iam/steps/train_lstm1d.sh
+++ egs/iam/steps/train_lstm1d.sh
@@ -11,18 +11,29 @@ SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 [ ! -f "$(pwd)/utils/parse_options.inc.sh" ] && \
     echo "Missing $(pwd)/utils/parse_options.inc.sh file!" >&2 && exit 1;
 
-height=128;
 batch_size=16;
+cnn_batch_norm=false;
+cnn_dropout=0;
+cnn_maxpool_size="2,2 2,2 1,2 1,2";
+cnn_num_features="12 24 48 96";
+cnn_type=leakyrelu;
+continue_train=false;
+gpu=1;
+height=128;
+linear_dropout=0.5;
 overwrite=false;
 partition="lines/aachen";
+rnn_num_layers=4;
+rnn_num_units=256;
+rnn_dropout=0.5;
 help_message="
 Usage: ${0##*/} [options]
 
 Options:
-  --height      : (type = integer, default = $height)
-                  Use images rescaled to this height.
   --batch_size  : (type = integer, default = $batch_size)
                   Batch size for training.
+  --height      : (type = integer, default = $height)
+                  Use images rescaled to this height.
   --overwrite   : (type = boolean, default = $overwrite)
                   Overwrite previously created files.
   --partition   : (type = string, default = \"$partition\")
@@ -50,37 +61,37 @@ num_syms="$(tail -n1 train/$ptype/syms.txt | awk '{ print $2 }')";
 mkdir -p "train/$partition";
 
 # Create model
-../../laia-create-model \
-  --cnn_type leakyrelu \
-  --cnn_kernel_size 3 \
-  --cnn_num_features 12 24 36 48 \
-  --cnn_maxpool_size  2,2 2,2 2,2 2,2 \
-  --cnn_batch_norm false \
-  --rnn_num_layers 4 \
-  --rnn_num_units 128 \
-  --rnn_dropout 0.5 \
-  --linear_dropout 0.5 \
-  --log_also_to_stderr info \
-  --log_file "train/$partition/lstm1d.log" \
-  --log_level info \
-  1 "$height" "$num_syms" "train/$partition/lstm1d.t7";
+[ "$continue_train" = true -a -s "train/$partition/lstm1d_h$height.t7" ] ||
+  ../../laia-create-model \
+    --cnn_batch_norm $cnn_batch_norm \
+    --cnn_dropout $cnn_dropout \
+    --cnn_kernel_size 3 \
+    --cnn_maxpool_size $cnn_maxpool_size \
+    --cnn_num_features $cnn_num_features \
+    --cnn_type $cnn_type \
+    --rnn_dropout "$rnn_dropout" \
+    --rnn_num_layers "$rnn_num_layers" \
+    --rnn_num_units "$rnn_num_units" \
+    --linear_dropout "$linear_dropout" \
+    --log_also_to_stderr info \
+    --log_file "train/$partition/lstm1d_h$height.log" \
+    --log_level info \
+    1 "$height" "$num_syms" "train/$partition/lstm1d_h$height.t7";
 
 # Train model
 ../../laia-train-ctc \
-  --use_distortions true \
   --batch_size "$batch_size" \
-  --progress_table_output "train/$partition/lstm1d.dat" \
-  --early_stop_epochs 50 \
-  --early_stop_threshold 0.05 \
-  --learning_rate 0.0005 \
-  --learning_rate_decay 0.98 \
-  --learning_rate_decay_after 50 \
-  --learning_rate_decay_min 0.0001 \
+  --continue_train "$continue_train" \
+  --use_distortions true \
+  --progress_table_output "train/$partition/lstm1d_h$height.dat" \
+  --early_stop_epochs 80 \
+  --learning_rate 0.0003 \
   --log_also_to_stderr info \
   --log_level info \
-  --log_file "train/$partition/lstm1d.log" \
+  --log_file "train/$partition/lstm1d_h$height.log" \
   --display_progress_bar true \
-  "train/$partition/lstm1d.t7" "train/$ptype/syms.txt" \
+  --gpu "$gpu" \
+  "train/$partition/lstm1d_h$height.t7" "train/$ptype/syms.txt" \
   "data/lists/$partition/tr_h${height}.lst" "data/lang/char/$partition/tr.txt" \
   "data/lists/$partition/va_h${height}.lst" "data/lang/char/$partition/va.txt";
 
