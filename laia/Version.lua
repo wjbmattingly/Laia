7925c7021307bd3b00de0d6cb12295a104b11727
Joan Puigcerver <joapuipe@gmail.com>
2017-03-03 10:07:16 +0100

    Fixed CachedBatcher, there was a bug that prevented images to be centered as they should.

--- laia/Version.lua
+++ laia/Version.lua
@@ -2,7 +2,7 @@ require 'laia.ClassWithOptions'
 
 local Version, Parent = torch.class('laia.Version', 'laia.ClassWithOptions')
 
-Version.DATE = '$Date: 2017-02-06 17:01:46 $'
+Version.DATE = '$Date: 2017-03-03 09:07:16 $'
 
 function Version:registerOptions(parser)
   parser:flag(
