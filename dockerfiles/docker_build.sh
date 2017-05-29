32ae64f5bbad9d6faaa737d09c9f642d451c29ef
Mauricio Villegas <mauricio_ville@yahoo.com>
2017-05-29 10:32:50 +0200

    Work on docker build

--- dockerfiles/docker_build.sh
+++ dockerfiles/docker_build.sh
@@ -2,39 +2,25 @@
 
 set -ex
 
-CUDA="8.0";
-sed 's|7\.5|8.0|g' -i $(find . -name Dockerfile);
+#CUDA="8.0"; CUDA_SED="s|7\.5|8.0|g";
+CUDA="7.5"; CUDA_SED="s|8\.0|7.5|g";
+sed "$CUDA_SED" -i Dockerfile torch-cuda/Dockerfile;
 
-#CUDA="7.5";
-#sed 's|8\.0|7.5|g' -i $(find . -name Dockerfile);
-
-OS="ubuntu16.04";
 #OS="ubuntu14.04";
-sed "s|ubuntu1.\.04|$OS|g" -i cuda-torch/deps1/Dockerfile;
+OS="ubuntu14.04";
+sed "s|ubuntu1.\.04|$OS|g" -i Dockerfile torch-cuda/Dockerfile;
 
 mkdir -p logs;
 
-### CUDA Torch dependencies 1 ###
-{ DS=$(date +%s);
-  docker build -t cuda-torch-deps1:$CUDA --build-arg CUDA=$CUDA cuda-torch/deps1
-  echo "time: $(( $(date +%s) - DS )) seconds";
-} 2>logs/cuda-torch-deps1-$CUDA.err >logs/cuda-torch-deps1-$CUDA.log;
-
-### CUDA Torch dependencies 2 ###
-{ DS=$(date +%s);
-  docker build -t cuda-torch-deps2:$CUDA --build-arg CUDA=$CUDA cuda-torch/deps2
-  echo "time: $(( $(date +%s) - DS )) seconds";
-} 2>logs/cuda-torch-deps2-$CUDA.err >logs/cuda-torch-deps2-$CUDA.log;
-
 ### CUDA Torch ###
 { DS=$(date +%s);
-  docker build -t cuda-torch:$CUDA --build-arg CUDA=$CUDA cuda-torch
+  nvidia-docker build -t torch-cuda:$CUDA-$OS --build-arg CUDA=$CUDA torch-cuda/all
   echo "time: $(( $(date +%s) - DS )) seconds";
-} 2>logs/cuda-torch-$CUDA.err >logs/cuda-torch-$CUDA.log;
+} 2>logs/torch-cuda$CUDA-$OS.err >logs/torch-cuda$CUDA-$OS.log;
 
 ### Laia ###
 REV=$(sed -rn '/^Version.DATE/{ s|.*Date: *([0-9]+)-([0-9]+)-([0-9]+).*|\1.\2.\3|; p; }' ../laia/Version.lua);
 { DS=$(date +%s);
-  docker build -t laia:${REV}-cuda$CUDA --build-arg CUDA=$CUDA .
+  nvidia-docker build -t laia:${REV}-cuda$CUDA-$OS --build-arg CUDA=$CUDA .;
   echo "time: $(( $(date +%s) - DS )) seconds";
-} 2>logs/laia-cuda$CUDA.err >logs/laia-cuda$CUDA.log;
+} 2>logs/laia-cuda$CUDA-$OS.err >logs/laia-cuda$CUDA-$OS.log;
