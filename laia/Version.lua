a190aed76815e292059aa00d209c4f21908b0d5c
Mauricio Villegas <mauricio_ville@yahoo.com>
2017-09-12 11:50:27 +0200

    Modifications to laia-docker so that custom lua scripts can also be executed

--- laia/Version.lua
+++ laia/Version.lua
@@ -2,7 +2,7 @@ require 'laia.ClassWithOptions'
 
 local Version, Parent = torch.class('laia.Version', 'laia.ClassWithOptions')
 
-Version.DATE = '$Date: 2017-09-07 14:58:12 $'
+Version.DATE = '$Date: 2017-09-12 09:50:27 $'
 
 function Version:registerOptions(parser)
   parser:flag(
