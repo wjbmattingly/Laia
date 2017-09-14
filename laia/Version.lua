38eb64580b3af1bdadd1bf0480d2fce3ed991fab
Mauricio Villegas <mauricio_ville@yahoo.com>
2017-09-14 12:19:00 +0200

    Implemented additive smoothing for unseen symbols in prior computation

--- laia/Version.lua
+++ laia/Version.lua
@@ -2,7 +2,7 @@ require 'laia.ClassWithOptions'
 
 local Version, Parent = torch.class('laia.Version', 'laia.ClassWithOptions')
 
-Version.DATE = '$Date: 2017-09-12 10:29:11 $'
+Version.DATE = '$Date: 2017-09-14 12:19:03 $'
 
 function Version:registerOptions(parser)
   parser:flag(
