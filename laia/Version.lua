1757e2e59eda4f0b36d429b4352d58c5f81e0511
Mauricio Villegas <mauricio_ville@yahoo.com>
2017-11-21 09:21:17 +0100

    Added missing lua files in rockspec

--- laia/Version.lua
+++ laia/Version.lua
@@ -2,7 +2,7 @@ require 'laia.ClassWithOptions'
 
 local Version, Parent = torch.class('laia.Version', 'laia.ClassWithOptions')
 
-Version.DATE = '$Date: 2017-11-09 09:29:00 $'
+Version.DATE = '$Date: 2017-11-21 08:21:17 $'
 
 function Version:registerOptions(parser)
   parser:flag(
